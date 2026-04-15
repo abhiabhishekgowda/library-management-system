c4cd7f1965a8000eca9206354561c4837b43706d
Abhishek.k <ceoabhi.k@gmail.com>
2026-04-15 17:15:52 +0530

    "day 3: added structured functions and implemented book view feature"

--- Library_manager.py
+++ Library_manager.py
@@ -1,36 +1,48 @@
-
 import sqlite3
 
-def initialize_library():
-    # 1. Connect to the database file
+def create_table():
     conn = sqlite3.connect("library.db")
-
-    # 2. Create a 'cursor'
     cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS BOOKS(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            author TEXT,
+            available INTEGER
+        )
+        """)
+    conn.commit()
 
-    # 3. Create the table if it doesn't exist
-    create_table = """
-    CREATE TABLE IF NOT EXISTS BOOKS(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        author TEXT,
-        available INTEGER
-    )
-    """
-    cursor.execute(create_table)
+def add_book(title,author):
+    with sqlite3.connect("library.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO BOOKS (title,author,available) VALUES (?,?,?)",
+            (title,author,1)
+        )
+        conn.commit()
+        print(f" Book '{title}' by {author} added successfully!") 
 
+def view_all_books():
+    with sqlite3.connect("library.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, title, author, available FROM BOOKS")
+        rows = cursor.fetchall()
+
+        print("\n--- YOUR PERMANENT LIBRARY ---")
+        if not rows:
+            print("NO books the library emty")
+        else:
+            for row in rows:
+                print(f"ID: {row[0]} | Title: {row[1]} | Author: {row[2]}")
+
+def main():
+    create_table()
     title = input("Enter title name: ")
     author = input("Enter author name: ")
-
-    query = """
-    INSERT INTO BOOKS(title, author, available)
-    VALUES (?, ?, ?)"""
-
-    cursor.execute(query,(title,author,1))
-    # 4. Commit and close
-    conn.commit()
-    conn.close()
-    print("Book added successfully")
+    add_book(title, author)
+    view_all_books()
+    print("🎉 Day one done!")
 
 if __name__ == "__main__":
-    initialize_library()
+    main()
